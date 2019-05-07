da9989a5b0056e0eeca0047b9c2fe8be80c2118a
Marta Niemiec <niemiec.marta@hotmail.com>
2019-05-07 11:35:58 +0200

    changed styling of toolbar and sidebar

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -15,24 +15,34 @@ class Layout extends Component {
     height: '100vh',
     backgroundColor: '#242A31',
     position: 'relative',
-    display: 'inline-block'
+    display: 'inline-block',
+    borderTop: '1px solid #444',
+    borderRight: '1px solid #444',
+    boxShadow: 'inset -7px 0 9px -7px #111'
   }
 
   let books = {
     height: '100vh',
     width: 'calc(100% - 150px)',
-    backgroundColor: '#fff',
+    backgroundColor: '#EEF0F2',
     position: 'relative',
     display: 'inline-block'
+  }
+
+  const main = {
+    display:'flex'
   }
 
     return (
       <Auxiliary>
         <Toolbar />
-        {/* <Sidebar /> */}
-        <div style={ sidebar }></div>
-        {/* <Books /> */}
-        <div style={ books }></div>
+        <main style={ main }>
+          {/* <Sidebar /> */}
+          <div style={ sidebar }></div>
+          {/* <Books /> */}
+          <div style={ books }></div>
+        </main>
+        
       </Auxiliary>
   );
   }
