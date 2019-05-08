9ec6af802c2f8ef6c0c3943ee6e038b6e6c7055e
Marta Niemiec <niemiec.marta@hotmail.com>
2019-05-08 13:12:38 +0200

    changed logo, removed fontAwsome link from HTML

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import classes from './Logo.module.scss'
+import classes from './Logo.module.scss';
+import styled from 'styled-components';
+import { Book } from 'styled-icons/fa-solid/Book';
 
-const logo = () => (
-    <i className={"fas fa-book " + classes.Logo}></i>
-)
+
+const logo = () => {
+    const LogoBook = styled(Book)`
+        width: 1.8rem;
+        height: 1.8rem;
+        color: #eee;
+        `
+    return (
+        <div className={classes.Logo}>
+            < LogoBook />
+        </div>
+        
+    )
+}
+
 
 export default logo;
