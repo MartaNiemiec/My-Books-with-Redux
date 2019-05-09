4c97b3ae8ff94927aebfad18d4fbe86bffe18825
Marta Niemiec <niemiec.marta@hotmail.com>
2019-05-09 23:26:15 +0200

    Updates

---
+++ precache-manifest.9ab022bbbe89d5b5941ce5c350bcc033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9002d521b02d61d4cc71d0e8cf62ab5",
+    "url": "/My-books/index.html"
+  },
+  {
+    "revision": "666abb54d7747077f89c",
+    "url": "/My-books/static/css/main.9f920edd.chunk.css"
+  },
+  {
+    "revision": "b7469e8b2a10800bb9e8",
+    "url": "/My-books/static/js/2.71f29fae.chunk.js"
+  },
+  {
+    "revision": "666abb54d7747077f89c",
+    "url": "/My-books/static/js/main.2f5d855e.chunk.js"
+  },
+  {
+    "revision": "897d200fc4ce72e6b5fa",
+    "url": "/My-books/static/js/runtime~main.05b624a2.js"
+  }
+]);
