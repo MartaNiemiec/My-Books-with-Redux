bafc3b0330b0f0b9cfc5eedacb403ef5cd0f05f2
Marta Niemiec <niemiec.marta@hotmail.com>
2019-05-13 15:21:28 +0200

    added displaying info when no books  matches

--- src/components/Books/BookList/Booklist.js
+++ src/components/Books/BookList/Booklist.js
@@ -1,25 +1,33 @@
     import React from 'react';
     import classes from './BookList.module.scss';
     import Book from './Book/Book';
+    import RedError from '../../UI/Icons/ErrorIcon.js'
     
     const bookList = ({searchBook, booksData}) => {
+      let searchedBooks;
 
-      
-    return (
-      <div className={classes.BookList}>
-        {
-          booksData.map((book, index) => {
+      // check if a books data is found
+      if (booksData) {
+        searchedBooks = booksData.map((book, index) => {
           return (
             <Book 
               key = {index}
               book = {book.volumeInfo}/>
           )
           })
-        }
-      </div>
-    
-    )
-    
+      } else {
+        searchedBooks =
+        <div className={classes.BookList__nomatches}>
+          <RedError />
+          <h2 className={classes.BookList__nomatches__info}>Sorry, no books matched your search. Please try again.</h2>
+        </div> 
+      }
+        
+      return (
+        <div className={classes.BookList}>
+          {searchedBooks}
+        </div>
+      )
     }
     
     export default bookList;
---
+++ src/components/UI/Icons/ErrorIcon.js
@@ -0,0 +1,11 @@
+
+import styled from 'styled-components';
+import { ErrorCircle } from 'styled-icons/boxicons-regular/ErrorCircle';
+
+const redError = styled(ErrorCircle)`
+  color: red;
+  width: 3rem;
+  height: 3rem;
+  `
+
+export default redError;
