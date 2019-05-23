faf055d8b451738f5f73c2b793abb4e938f68b40
Marta Niemiec <niemiec.marta@hotmail.com>
2019-05-23 18:33:45 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/My-books/precache-manifest.fd6250b6f58e5bfba8106bebcd68b387.js"
+  "/My-books/precache-manifest.c1cfa1654caaf1347136c0edb99543e2.js"
 );
 
 self.addEventListener('message', (event) => {
