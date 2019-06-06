b57066c3d74a2a377f2e48a10e55e1ff9767c4bf
Marta Niemiec <niemiec.marta@hotmail.com>
2019-06-06 19:41:14 +0200

    added purchase button to the book modal

--- src/components/Books/BookList/Book/BookModal/BookModal.js
+++ src/components/Books/BookList/Book/BookModal/BookModal.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Modal from '../../../../UI/Modal/Modal';
 import classes from './BookModal.module.scss';
+import PurchaseButton from '../../../../UI/PurchaseButton/PurchaseButton';
 
 const bookModal = (props) => {
-
   const { book, show, modalClosed, fullAuthors } = props;
   let bookImage;
   
@@ -11,25 +11,24 @@ const bookModal = (props) => {
   ? bookImage = book.volumeInfo.imageLinks.smallThumbnail
   : bookImage = "";
 
-
   return (
-      <Modal show={show} 
-             modalClosed={modalClosed}>
-        <div className={classes.BookModal}>
-          <img src={bookImage} className={classes.BookModal__image} alt=""/>
-          <div className={classes.BookModal__content}>
-            <h2>{book.volumeInfo.title}</h2>
-            <h4>{fullAuthors}</h4>
-            <div>{book.volumeInfo.description}</div>
-          </div>
+    <Modal show={show} 
+           modalClosed={modalClosed}>
+      <div className={classes.BookModal}>
+        <div className={classes.BookModal__left}>
+          <img src={bookImage} 
+               className={classes.BookModal__left__image} 
+               alt="Book cover"/>
+          <PurchaseButton book={book}/>
         </div>
-        
-      </Modal>
-
-    
-  
-);
+        <div className={classes.BookModal__content}>
+          <h2>{book.volumeInfo.title}</h2>
+          <h4>{fullAuthors}</h4>
+          <div>{book.volumeInfo.description}</div>
+        </div>
+      </div>
+    </Modal>
+  );
 }
-
 
 export default bookModal;
---
+++ src/components/UI/PurchaseButton/PurchaseButton.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import classes from './PurchaseButton.module.scss';
+
+const purchaseButton = (props) => {
+  const book = props.book.saleInfo;
+  const bookSaleability = book.saleability;
+  let buttonClassName, buttonContent, link, listPrice, retailPrice;
+
+  if (bookSaleability === "FREE") {
+    buttonClassName = "Button__forFree";
+    buttonContent = "FREE E-Book";
+    link = book.buyLink;
+  } else if (bookSaleability === "FOR_SALE") {
+    buttonClassName = "Button__forSale";
+    retailPrice = book.retailPrice.amount + ' ' + book.retailPrice.currencyCode;
+    listPrice = book.listPrice.amount + ' ' + book.listPrice.currencyCode;
+    link = book.buyLink;
+    buttonContent = <div style={{display: "flex", alignItems: "center"}}>
+                      <span>BUY</span>
+                      <div className={classes.Button__prices}>
+                        <span className={classes.Button__listPrice}>{listPrice}</span>
+                        <span className={classes.Button__retailPrice}>{retailPrice}</span>
+                      </div>
+                    </div>
+  } else if (bookSaleability === "NOT_FOR_SALE") {
+    buttonClassName = "Button__notForSale"
+    buttonContent = "NOT FOR SALE";
+    link = "";
+  }
+
+  return (
+    <button className={classes[buttonClassName]}>
+      <a target="_blank" 
+        rel="noopener noreferrer"
+        href={link} 
+        title="Buy in Google Play" 
+        className={classes.Button__link}>{buttonContent}</a> 
+    </button>
+  )
+}
+
+
+export default purchaseButton;
